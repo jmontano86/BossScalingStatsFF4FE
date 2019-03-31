4473fe9294555d6c46cb6142ae048f45a7000044
jmontano86 <jmontano86@gmail.com>
2019-03-31 08:53:17 -0700

    Initial commit

---
+++ Enemy.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BossScalingStatsFF4FE
+{
+    class Enemy
+    {
+        private string _Battle;
+        private string _Slot;
+        private string _Name;
+        private int _Level;
+        private int _HP;
+        private int _XP;
+        private int _GP;
+        private int _AtkMult;
+        private int _Hit; //hit percentage
+        private int _Atk;
+        private int _DefMult;
+        private int _Evade; //evade percentage
+        private int _Defense;
+        private int _MagicDefMult;
+        private int _MagicEvade; //magic evade percentage
+        private int _MagicDefense;
+        private int _MinSpeed;
+        private int _MaxSpeed;
+        private int _Spellpower;
+        private string _Misc;
+
+        public string eName
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+
+        public int eLevel
+        {
+            get { return _Level; }
+            set { _Level = value; }
+        }
+
+        public int eHP
+        {
+            get { return _HP; }
+            set { _HP = value; }
+        }
+
+        public int eXP
+        {
+            get { return _XP; }
+            set { _XP = value; }
+        }
+        public int eGP
+        {
+            get { return _GP; }
+            set { _GP = value; }
+        }
+        public int eAttackMultiplier
+        {
+            get { return _AtkMult; }
+            set { _AtkMult = value; }
+        }
+        public int eHitPercentage
+        {
+            get { return _Hit; }
+            set { _Hit = value; }
+        }
+        public int eAttack
+        {
+            get { return _Atk; }
+            set { _Atk = value; }
+        }
+        public int eDefenseMultiplier
+        {
+            get { return _DefMult; }
+            set { _DefMult = value; }
+        }
+        public int eEvade
+        {
+            get { return _Evade; }
+            set { _Evade = value; }
+        }
+        public int eDefense
+        {
+            get { return _Defense; }
+            set { _Defense = value; }
+        }
+        public int eMagicDefenseMultiplier
+        {
+            get { return _MagicDefMult; }
+            set { _MagicDefMult = value; }
+        }
+        public int eMagicEvade
+        {
+            get { return _MagicEvade; }
+            set { _MagicEvade = value; }
+        }
+        public int eMagicDefense
+        {
+            get { return _MagicDefense; }
+            set { _MagicDefense = value; }
+        }
+        public int eMinSpeed
+        {
+            get { return _MinSpeed; }
+            set { _MinSpeed = value; }
+        }
+        public int eMaxSpeed
+        {
+            get { return _MaxSpeed; }
+            set { _MaxSpeed = value; }
+        }
+        public int eSpellpower
+        {
+            get { return _Spellpower; }
+            set { _Spellpower = value; }
+        }
+        public string eMisc
+        {
+            get { return _Misc; }
+            set { _Misc = value; }
+        }
+
+        public string eBattle { get => _Battle; set => _Battle = value; }
+        public string eSlot { get => _Slot; set => _Slot = value; }
+
+        public static List<Enemy> loadAllEnemies()
+        {
+            List<Enemy> enemyList = new List<Enemy>();
+            using(var reader = new StreamReader("EnemyData.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    Enemy e = new Enemy();
+                    e.eBattle = values[0];
+                    e.eSlot = values[1];
+                    e.eName = values[2];
+                    e.eLevel = Convert.ToInt32(values[3]);
+                    e.eHP = Convert.ToInt32(values[4]);
+                    e.eXP = Convert.ToInt32(values[5]);
+                    e.eGP = Convert.ToInt32(values[6]);
+                    e.eAttackMultiplier = Convert.ToInt32(values[7]);
+                    e.eHitPercentage = Convert.ToInt32(values[8]);
+                    e.eAttack = Convert.ToInt32(values[9]);
+                    e.eDefenseMultiplier = Convert.ToInt32(values[10]);
+                    e.eEvade = Convert.ToInt32(values[11]);
+                    e.eDefense = Convert.ToInt32(values[12]);
+                    e.eMagicDefenseMultiplier = Convert.ToInt32(values[13]);
+                    e.eMagicEvade = Convert.ToInt32(values[14]);
+                    e.eMagicDefense = Convert.ToInt32(values[15]);
+                    e.eMinSpeed = Convert.ToInt32(values[16]);
+                    e.eMaxSpeed = Convert.ToInt32(values[17]);
+                    if (values[18] != "") {
+                        e.eSpellpower = Convert.ToInt32(values[18]);
+                    } else
+                    {
+                        e.eSpellpower = 0;
+                    }
+
+                    enemyList.Add(e);
+                }
+            }
+            return enemyList;
+        }
+    }
+
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BossScalingStatsFF4FE
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Enemy> enemySelections;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void processSelections()
+        {
+            resetVisibility();
+            List<Enemy> enemyList = new List<Enemy>();
+            var selectedEnemy = from Enemy in enemySelections
+                                where Enemy.eBattle == bossComboBox.SelectedItem.ToString() &&
+                                Enemy.eSlot == slotComboBox.SelectedItem.ToString()
+                                select Enemy;
+            enemyList = selectedEnemy.ToList();
+            if(enemyList.Count == 1)
+            {
+                Enemy en = enemyList[0];
+                populateGrid1(en);
+                return;
+            }
+            if(enemyList.Count == 2)
+            {
+                Enemy en1 = enemyList[0];
+                Enemy en2 = enemyList[1];
+                Enemy2GroupBox.Visibility = Visibility.Visible;
+                populateGrid1(en1);
+                populateGrid2(en2);
+                return;
+            }
+            if (enemyList.Count == 3)
+            {
+                Enemy en1 = enemyList[0];
+                Enemy en2 = enemyList[1];
+                Enemy en3 = enemyList[2];
+                Enemy2GroupBox.Visibility = Visibility.Visible;
+                Enemy3GroupBox.Visibility = Visibility.Visible;
+                populateGrid1(en1);
+                populateGrid2(en2);
+                populateGrid3(en3);
+                return;
+            }
+            if (enemyList.Count == 4)
+            {
+                Enemy en1 = enemyList[0];
+                Enemy en2 = enemyList[1];
+                Enemy en3 = enemyList[2];
+                Enemy en4 = enemyList[3];
+                Enemy2GroupBox.Visibility = Visibility.Visible;
+                Enemy3GroupBox.Visibility = Visibility.Visible;
+                Enemy4GroupBox.Visibility = Visibility.Visible;
+                populateGrid1(en1);
+                populateGrid2(en2);
+                populateGrid3(en3);
+                populateGrid4(en4);
+                return;
+            }
+            if (enemyList.Count == 5)
+            {
+                Enemy en1 = enemyList[0];
+                Enemy en2 = enemyList[1];
+                Enemy en3 = enemyList[2];
+                Enemy en4 = enemyList[3];
+                Enemy en5 = enemyList[4];
+                Enemy2GroupBox.Visibility = Visibility.Visible;
+                Enemy3GroupBox.Visibility = Visibility.Visible;
+                Enemy4GroupBox.Visibility = Visibility.Visible;
+                Enemy5GroupBox.Visibility = Visibility.Visible;
+                populateGrid1(en1);
+                populateGrid2(en2);
+                populateGrid3(en3);
+                populateGrid4(en4);
+                populateGrid5(en5);
+                return;
+            }
+            if (enemyList.Count == 6)
+            {
+                Enemy en1 = enemyList[0];
+                Enemy en2 = enemyList[1];
+                Enemy en3 = enemyList[2];
+                Enemy en4 = enemyList[3];
+                Enemy en5 = enemyList[4];
+                Enemy en6 = enemyList[5];
+                Enemy2GroupBox.Visibility = Visibility.Visible;
+                Enemy3GroupBox.Visibility = Visibility.Visible;
+                Enemy4GroupBox.Visibility = Visibility.Visible;
+                Enemy5GroupBox.Visibility = Visibility.Visible;
+                Enemy6GroupBox.Visibility = Visibility.Visible;
+                populateGrid1(en1);
+                populateGrid2(en2);
+                populateGrid3(en3);
+                populateGrid4(en4);
+                populateGrid5(en5);
+                populateGrid6(en6);
+                return;
+            }
+
+
+        }
+
+        private void resetVisibility()
+        {
+            Enemy2GroupBox.Visibility = Visibility.Hidden;
+            Enemy3GroupBox.Visibility = Visibility.Hidden;
+            Enemy4GroupBox.Visibility = Visibility.Hidden;
+            Enemy5GroupBox.Visibility = Visibility.Hidden;
+            Enemy6GroupBox.Visibility = Visibility.Hidden;
+        }
+
+        private void populateGrid1(Enemy en)
+        {
+            Enemy1GroupBox.Header = en.eName;
+            Level.Text = en.eLevel.ToString();
+            HP.Text = en.eHP.ToString();
+            XP.Text = en.eXP.ToString();
+            GP.Text = en.eGP.ToString();
+            AtkMult.Text = en.eAttackMultiplier.ToString();
+            Hit.Text = en.eHitPercentage.ToString();
+            Attack.Text = en.eAttack.ToString();
+            DefMult.Text = en.eDefenseMultiplier.ToString();
+            Evade.Text = en.eEvade.ToString();
+            Defense.Text = en.eDefense.ToString();
+            MagDefMult.Text = en.eMagicDefenseMultiplier.ToString();
+            MagEvade.Text = en.eMagicEvade.ToString();
+            MagicDefense.Text = en.eMagicDefense.ToString();
+            MinSpeed.Text = en.eMinSpeed.ToString();
+            MaxSpeed.Text = en.eMaxSpeed.ToString();
+            Spellpower.Text = en.eSpellpower.ToString();
+        }
+        private void populateGrid2(Enemy en)
+        {
+            Enemy2GroupBox.Header = en.eName;
+            Level2.Text = en.eLevel.ToString();
+            HP2.Text = en.eHP.ToString();
+            XP2.Text = en.eXP.ToString();
+            GP2.Text = en.eGP.ToString();
+            AtkMult2.Text = en.eAttackMultiplier.ToString();
+            Hit2.Text = en.eHitPercentage.ToString();
+            Attack2.Text = en.eAttack.ToString();
+            DefMult2.Text = en.eDefenseMultiplier.ToString();
+            Evade2.Text = en.eEvade.ToString();
+            Defense2.Text = en.eDefense.ToString();
+            MagDefMult2.Text = en.eMagicDefenseMultiplier.ToString();
+            MagEvade2.Text = en.eMagicEvade.ToString();
+            MagicDefense2.Text = en.eMagicDefense.ToString();
+            MinSpeed2.Text = en.eMinSpeed.ToString();
+            MaxSpeed2.Text = en.eMaxSpeed.ToString();
+            Spellpower2.Text = en.eSpellpower.ToString();
+        }
+        private void populateGrid3(Enemy en)
+        {
+            Enemy3GroupBox.Header = en.eName;
+            Level3.Text = en.eLevel.ToString();
+            HP3.Text = en.eHP.ToString();
+            XP3.Text = en.eXP.ToString();
+            GP3.Text = en.eGP.ToString();
+            AtkMult3.Text = en.eAttackMultiplier.ToString();
+            Hit3.Text = en.eHitPercentage.ToString();
+            Attack3.Text = en.eAttack.ToString();
+            DefMult3.Text = en.eDefenseMultiplier.ToString();
+            Evade3.Text = en.eEvade.ToString();
+            Defense3.Text = en.eDefense.ToString();
+            MagDefMult3.Text = en.eMagicDefenseMultiplier.ToString();
+            MagEvade3.Text = en.eMagicEvade.ToString();
+            MagicDefense3.Text = en.eMagicDefense.ToString();
+            MinSpeed3.Text = en.eMinSpeed.ToString();
+            MaxSpeed3.Text = en.eMaxSpeed.ToString();
+            Spellpower3.Text = en.eSpellpower.ToString();
+        }
+        private void populateGrid4(Enemy en)
+        {
+            Enemy4GroupBox.Header = en.eName;
+            Level4.Text = en.eLevel.ToString();
+            HP4.Text = en.eHP.ToString();
+            XP4.Text = en.eXP.ToString();
+            GP4.Text = en.eGP.ToString();
+            AtkMult4.Text = en.eAttackMultiplier.ToString();
+            Hit4.Text = en.eHitPercentage.ToString();
+            Attack4.Text = en.eAttack.ToString();
+            DefMult4.Text = en.eDefenseMultiplier.ToString();
+            Evade4.Text = en.eEvade.ToString();
+            Defense4.Text = en.eDefense.ToString();
+            MagDefMult4.Text = en.eMagicDefenseMultiplier.ToString();
+            MagEvade4.Text = en.eMagicEvade.ToString();
+            MagicDefense4.Text = en.eMagicDefense.ToString();
+            MinSpeed4.Text = en.eMinSpeed.ToString();
+            MaxSpeed4.Text = en.eMaxSpeed.ToString();
+            Spellpower4.Text = en.eSpellpower.ToString();
+        }
+        private void populateGrid5(Enemy en)
+        {
+            Enemy5GroupBox.Header = en.eName;
+            Level5.Text = en.eLevel.ToString();
+            HP5.Text = en.eHP.ToString();
+            XP5.Text = en.eXP.ToString();
+            GP5.Text = en.eGP.ToString();
+            AtkMult5.Text = en.eAttackMultiplier.ToString();
+            Hit5.Text = en.eHitPercentage.ToString();
+            Attack5.Text = en.eAttack.ToString();
+            DefMult5.Text = en.eDefenseMultiplier.ToString();
+            Evade5.Text = en.eEvade.ToString();
+            Defense5.Text = en.eDefense.ToString();
+            MagDefMult5.Text = en.eMagicDefenseMultiplier.ToString();
+            MagEvade5.Text = en.eMagicEvade.ToString();
+            MagicDefense5.Text = en.eMagicDefense.ToString();
+            MinSpeed5.Text = en.eMinSpeed.ToString();
+            MaxSpeed5.Text = en.eMaxSpeed.ToString();
+            Spellpower5.Text = en.eSpellpower.ToString();
+        }
+        private void populateGrid6(Enemy en)
+        {
+            Enemy6GroupBox.Header = en.eName;
+            Level6.Text = en.eLevel.ToString();
+            HP6.Text = en.eHP.ToString();
+            XP6.Text = en.eXP.ToString();
+            GP6.Text = en.eGP.ToString();
+            AtkMult6.Text = en.eAttackMultiplier.ToString();
+            Hit6.Text = en.eHitPercentage.ToString();
+            Attack6.Text = en.eAttack.ToString();
+            DefMult6.Text = en.eDefenseMultiplier.ToString();
+            Evade6.Text = en.eEvade.ToString();
+            Defense6.Text = en.eDefense.ToString();
+            MagDefMult6.Text = en.eMagicDefenseMultiplier.ToString();
+            MagEvade6.Text = en.eMagicEvade.ToString();
+            MagicDefense6.Text = en.eMagicDefense.ToString();
+            MinSpeed6.Text = en.eMinSpeed.ToString();
+            MaxSpeed6.Text = en.eMaxSpeed.ToString();
+            Spellpower6.Text = en.eSpellpower.ToString();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Load data into list
+            enemySelections = new List<Enemy>();
+            enemySelections = Enemy.loadAllEnemies();
+            resetVisibility();
+
+            //Load distinct Data into Battle/Slot positions
+            foreach (var item in enemySelections.Select(x => x.eBattle).Distinct())
+            {
+                bossComboBox.Items.Add(item);
+            }
+
+            foreach(var item in enemySelections.Select(x => x.eSlot).Distinct())
+            {
+                slotComboBox.Items.Add(item);
+            }
+        }
+
+        private void BossComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (bossComboBox.SelectedIndex != -1 && slotComboBox.SelectedIndex != -1)
+            {
+                    processSelections();
+            }
+        }
+
+        private void SlotComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (bossComboBox.SelectedIndex != -1 && slotComboBox.SelectedIndex != -1)
+            {
+                processSelections();
+            }
+        }
+    }
+}
